9f668299dcd19df8e4b0dc258d831821563de628
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-05-28 19:42:28 +0530

    Add files via upload

---
+++ OutPut_Base.js
@@ -0,0 +1,36 @@
+// console.log(null == undefined); // true
+// console.log(null === undefined); // false
+// console.log(typeof 42); //number
+// console.log(3 + 2 + "7"); //'57'
+// console.log("1" - -"1"); //2
+// console.log(1 - -"1"); //2
+// console.log(2 * 3 + "7"); //67
+// console.log({} + []);
+// console.log("2" > "12"); //true
+// console.log("2" < "12"); //false
+// console.log("2" > 12); //false
+// console.log("2" < 12); //true
+// console.log(+"5" + 3);
+// console.log(typeof NaN);
+// console.log(2 + 5 + "8");
+// console.log("2" + "5" + 3);
+// console.log(undefined + 5);
+// console.log("hello".charAt(2));
+// console.log("hello".toUpperCase());
+// console.log(1 / 0); //infinity
+// console.log(-1 / 0); // -infinity
+// console.log(Infinity === Infinity);
+console.log(NaN === NaN);
+console.log(Boolean(NaN));
+console.log("hello".indexOf("l"));
+console.log("hello".replace("l", "L"));
+console.log("hello".split(""));
+console.log(Math.floor(2.5));
+console.log(Math.ceil(2.2));
+console.log(Math.round(2.5));
+console.log(parseInt("10.5"));
+console.log(parseFloat(10.9));
+console.log(("b" + "a" + +"a" + "a").toLowerCase());
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+console.log(1 + 1 == 2 || 10 * 10 > 50);
+console.log((true && false) || (false && true) || !(false && false));
